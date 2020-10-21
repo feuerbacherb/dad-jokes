39958aa1e8eb99fe307a2846ea32847822bfbd5d
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-10-21 08:45:14 -0500

    added formatting to JokeList, which includes buttons and button handling of the Joke component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Joke from './Joke';
+import { v4 } from 'uuid';
+import './JokeList.css';
 
 export default class JokeList extends Component {
 	static defaultProps = {
@@ -18,16 +21,44 @@ export default class JokeList extends Component {
 		let jokes = [];
 		while (jokes.length < this.props.numJokesToGet) {
 			let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
-			jokes.push(res.data.joke);
+			jokes.push({ id: v4(), text: res.data.joke, votes: 0 });
 		}
 		this.setState({ jokes: jokes });
+	}
+
+	handleVote(id, delta) {
+		this.setState((st) => ({
+			jokes : st.jokes.map(
+				(j) =>
+
+						j.id === id ? { ...j, votes: j.votes + delta } :
+						j
+			),
+		}));
 	}
 
 	render() {
 		return (
 			<div className='JokeList'>
-				<h1>Dad Jokes</h1>
-				<div className='JokeList-jokes'>{this.state.jokes.map((j) => <div>{j}</div>)}</div>
+				<div className='JokeList-sidebar'>
+					<h1 className='JokeList-title'>
+						<span>Dad</span> Jokes
+					</h1>
+					<img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+					<button className='JokeList-getmore'>New Jokes</button>
+				</div>
+
+				<div className='JokeList-jokes'>
+					{this.state.jokes.map((j) => (
+						<Joke
+							key={j.id}
+							votes={j.votes}
+							text={j.text}
+							upvote={() => this.handleVote(j.id, 1)}
+							downvote={() => this.handleVote(j.id, -1)}
+						/>
+					))}
+				</div>
 			</div>
 		);
 	}
